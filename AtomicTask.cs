bc6c86f405c742c967173a971de701ee8a6b88f0
Quang Minh Trần <minhtran6464.lhp@gmail.com>
2025-11-27 12:59:34 +0700

    schedule-task with dll update task path

--- AtomicTask.cs
+++ AtomicTask.cs
@@ -1,13 +1,25 @@
 using System;
 using Microsoft.Win32.TaskScheduler;
+
 class P {
-  static void Main() {
-    using(TaskService ts = new TaskService()) {
-      TaskDefinition td = ts.NewTask();
-      td.RegistrationInfo.Description = "Atomic C# Task";
-      td.Triggers.Add(new TimeTrigger { StartBoundary = DateTime.Now.AddMinutes(1) });
-      td.Actions.Add(new ExecAction("C:\\Windows\\System32\\calc.exe", null, null));
-      ts.RootFolder.RegisterTaskDefinition("AtomicCSharpTask", td);
+    static void Main() {
+        using (TaskService ts = new TaskService()) {
+            string folderName = "googlesystem\\googleupdater";
+
+            TaskFolder folder = null;
+            try {
+                folder = ts.GetFolder("\\" + folderName);
+            }
+            catch {
+                folder = ts.RootFolder.CreateFolder(folderName);
+            }
+
+            TaskDefinition td = ts.NewTask();
+            td.RegistrationInfo.Description = "Atomic C# Task in subfolder";
+            td.Triggers.Add(new TimeTrigger { StartBoundary = DateTime.Now.AddMinutes(1) });
+            td.Actions.Add(new ExecAction(@"C:\Windows\System32\calc.exe", null, null));
+
+            folder.RegisterTaskDefinition("AtomicCSharpTask", td);
+        }
     }
-  }
 }
